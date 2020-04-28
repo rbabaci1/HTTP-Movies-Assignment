3ab9b26462ab16ab1b9dceefbdbe96a679593e19
rabah babaci <4rabah@gmail.com>
2020-04-28 11:54:57 -0700

    clean and analyse the components to get a better understanding

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { Route } from "react-router-dom";
-import SavedList from "./Movies/SavedList";
-import MovieList from "./Movies/MovieList";
-import Movie from "./Movies/Movie";
+import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import axios from 'axios';
 
 const App = () => {
@@ -11,12 +11,12 @@ const App = () => {
 
   const getMovieList = () => {
     axios
-      .get("http://localhost:5000/api/movies")
-      .then(res => setMovieList(res.data))
-      .catch(err => console.log(err.response));
+      .get('http://localhost:5000/api/movies')
+      .then((res) => setMovieList(res.data))
+      .catch((err) => console.error(err));
   };
 
-  const addToSavedList = movie => {
+  const addToSavedList = (movie) => {
     setSavedList([...savedList, movie]);
   };
 
@@ -28,11 +28,11 @@ const App = () => {
     <>
       <SavedList list={savedList} />
 
-      <Route exact path="/">
+      <Route exact path='/'>
         <MovieList movies={movieList} />
       </Route>
 
-      <Route path="/movies/:id">
+      <Route path='/movies/:id'>
         <Movie addToSavedList={addToSavedList} />
       </Route>
     </>
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import MovieCard from "./MovieCard";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
@@ -27,10 +27,10 @@ function Movie({ addToSavedList }) {
   }
 
   return (
-    <div className="save-wrapper">
+    <div className='save-wrapper'>
       <MovieCard movie={movie} />
 
-      <div className="save-button" onClick={saveMovie}>
+      <div className='save-button' onClick={saveMovie}>
         Save
       </div>
     </div>
